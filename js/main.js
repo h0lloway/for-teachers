69c3391b802a9c6bb5704bd432e01dd7b9558183
h0lloway <gvozdb27@gmail.com>
2022-04-02 23:12:57 +0300

    work updated
    adaptiv, PP, warnings

--- js/main.js
+++ js/main.js
@@ -1,37 +1,3 @@
-// HEADER // HEADER // HEADER // HEADER // HEADER // HEADER 
-
-// let lastScroll = 0;
-// const defaultOffset = 200;
-// const header = document.querySelector('.header');
-
-// const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-// const containHide = () => header.classList.contains('hide');
-
-// window.addEventListener('scroll', () => {
-
-
-//   if (scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
-//     // scroll down
-//     header.classList.add('hide');
-//   }
-//   else if (scrollPosition() < lastScroll && containHide()) {
-//     // scroll up
-//     header.classList.remove('hide');
-//   }
-
-
-//   lastScroll = scrollPosition();
-// });
-
-// ПЛАВНЫЕ ЯКОРЯ
-
-// $('a[href*="#"]').on('click', function () {
-//   $('html, body').animate({
-//     scrollTop: $($.attr(this, 'href')).offset().top
-//   }, 600);
-//   return false;
-// });
-
 // BURGER
 
 $(document).ready(function () {
@@ -41,91 +7,14 @@ $(document).ready(function () {
   });
 });
 
-// SWICH THEME
 
-
-// let lightTheme = document.querySelector(".swich-theme-btn-light");
-// let darkTheme = document.querySelector(".swich-theme-btn-dark");
-
-
-// lightTheme.addEventListener("click", function () {
-//   this.style.display = "none";
-//   darkTheme.style.display = "block";
-
-
-//   darkTheme.addEventListener("click", function () {
-//     this.style.display = "none";
-//     lightTheme.style.display = "block";
-//   });
-// });
-
-
-// const toggleThemeBtn = document.getElementById('theme-btn');
-// const toggleThemeImg = document.getElementById('theme-img');
-
-
-// function setDarkTheme() {
-//   document.body.classList.add('dark')
-//   toggleThemeImg.src = 'img/sun.png'
-//   localStorage.theme = 'dark'
-// }
-
-// function setLightTheme() {
-//   document.body.classList.remove('dark')
-//   toggleThemeImg.src = 'img/moon.png'
-//   localStorage.theme = 'light'
-// }
-
-// toggleThemeBtn.addEventListener('click', () => {
-
-//   if (document.body.classList.contains('dark')) setLightTheme()
-//   else setDarkTheme()
-// })
-
-// if (localStorage.theme === 'dark') setDarkTheme()
-
-
-// TOOLTIPS
-
-// tippy('.js-tooltip', {
-//   theme: 'projects-tooltip',
-//   animation: 'fade',
-//   delay: 300,
-//   placement: "top",
-//   allowHTML: !0,
-//   role: "tooltip",
-//   trigger: "mouseenter focus click",
-//   hideOnClick: !0,
-//   maxWidth: 250
-// });
-
-
-
-
-// РАСКРЫТЬ КАРТОЧКИ
-
-// const hifgRatingItems = gsap.timeline({ paused: true })
-
-// hifgRatingItems
-//   .to(".hidden", { duration: .2, opacity: 1, visibility: "visible", display: "inline-flex" })
-
-// document.querySelector(".more-item-btn").addEventListener("click", function () {
-//   document.querySelector(".close-item-btn").classList.add("visible")
-//   document.querySelector(".more-item-btn").classList.add("hidden")
-//   hifgRatingItems.timeScale(1).play()
-// })
-// document.querySelector(".close-item-btn").addEventListener("click", function () {
-//   document.querySelector(".close-item-btn").classList.remove("visible")
-//   document.querySelector(".more-item-btn").classList.remove("hidden")
-//   hifgRatingItems.timeScale(2).reverse()
-// })
-
+// SWIPERs
 
 const congratsSwiper = new Swiper('.congrats__swiper', {
   // Optional parameters
   slidesPerView: 5,
   loop: true,
-  spaceBetween: 10,
+  // spaceBetween: 10,
 
   // If we need pagination
   pagination: {
@@ -138,6 +27,28 @@ const congratsSwiper = new Swiper('.congrats__swiper', {
     nextEl: '.congrats__swiper-button-next',
     prevEl: '.congrats__swiper-button-prev',
   },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+
+    550: {
+      slidesPerView: 2,
+    },
+
+    750: {
+      slidesPerView: 3,
+    },
+
+    999: {
+      slidesPerView: 4,
+    },
+
+    1200: {
+      slidesPerView: 5,
+    }
+  }
 });
 
 
@@ -159,5 +70,40 @@ const presentSwiper = new Swiper('.present__swiper', {
     nextEl: '.present__swiper-button-next',
     prevEl: '.present__swiper-button-prev',
   },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+
+    550: {
+      slidesPerView: 2,
+      centeredSlides: false,
+      spaceBetween: 40,
+    },
+
+    750: {
+      slidesPerView: 3,
+      loop: true,
+      // spaceBetween: 20,
+      centeredSlides: true,
+
+    },
+
+    999: {
+      slidesPerView: 3,
+      loop: true,
+      // spaceBetween: 20,
+      centeredSlides: true,
+    },
+
+    1200: {
+      slidesPerView: 3,
+      loop: true,
+      // spaceBetween: 20,
+      centeredSlides: true,
+    }
+  }
 
 });
